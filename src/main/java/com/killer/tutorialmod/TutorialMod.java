e7aed920ebf9fa198225263c3888025fce5ef52c
Killer88967 <96306228+Killer88967@users.noreply.github.com>
2025-12-09 16:30:45 +0000

    New items

---
+++ src/main/java/com/killer/tutorialmod/TutorialMod.java
@@ -0,0 +1,18 @@
+package com.killer.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.killer.tutorialmod.item.ModItems;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItems.registerModItems();
+	}
+}
